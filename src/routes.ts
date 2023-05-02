39c45d2b0e4b432b840ec8c56a7de688c38b5bec
Edson Bastos <abastos@uolin.com>
2023-05-02 20:58:50 -0300

    feat: instalado multer

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router} from 'express';
+import multer from 'multer';
+
 import { UserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
@@ -9,7 +11,11 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import uploadConfig from './config/multer';
+
 export const router = Router();
+
+const upload = multer(uploadConfig.upload('./tmp'));
 
 // Rotas User
 router.post('/users', new UserController().handle);
@@ -21,4 +27,4 @@ router.post('/category',isAuthenticated, new CreateCategoryController().handle);
 router.get('/category',isAuthenticated, new ListCategoryController().handle );
 
 // Rotas Produtos
-router.post('/product',isAuthenticated, new CreateProductController().handle);
+router.post('/product',isAuthenticated,upload.single('file'), new CreateProductController().handle);
